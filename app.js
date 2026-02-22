d51c7965f771f129c3f937af35c24e6d40ca9456
Mj <mjomamogho@gmail.com>
2026-02-22 00:25:29 +0000

    fix general note styling, set up JS items

---
+++ app.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+const dateContainer = document.getElementById("dateContainer");
+const addBtn = document.getElementById("addButton");
+const closeBtn = document.getElementById("closeButton");
+const note = document.getElementById("noteContent");
+
+addBtn.addEventListener("click", addNewNote);
+closeBtn.addEventListener("click", closeNote)
+
+function addNewNote(){
+    console.log("newNote");
+};
+function closeNote(){
+
+}
